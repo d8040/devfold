fa9f695ab1eae579f83f6c5d46516754d2f44f89
d8040 <d8040@nate.com>
2021-03-11 22:01:50 +0900

    DB 생성

---
+++ db.sql
@@ -0,0 +1,108 @@
+DROP DATABASE IF EXISTS devfolio;
+CREATE DATABASE devfolio;
+USE devfolio;
+
+# 회원 정보 테이블 생성
+CREATE TABLE `member`(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(30) UNIQUE NOT NULL,
+    loginPw VARCHAR(100) NOT NULL,
+    authLevel SMALLINT(2) UNIQUE DEFAULT 3 NOT NULL COMMENT '(3=일반, 5=기업, 7=관리자)',
+    authKey CHAR(30) NOT NULL, 
+    `name` CHAR(30) NOT NULL,
+    `nickname` CHAR(30) NOT NULL,
+    `email` CHAR(100) NOT NULL,
+    `cellphoneNo` CHAR(20) NOT NULL,
+    birth DATETIME NOT NULL,
+    sex CHAR(10) NOT NULL
+);
+
+ALTER TABLE `member` ADD UNIQUE INDEX (authKey);
+
+# 게시판 테이블 생성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `code` CHAR(10) NOT NULL UNIQUE,
+    `name` CHAR(10) NOT NULL UNIQUE
+);
+
+# 게시물 테이블 생성
+CREATE TABLE article (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    boardId INT(10) UNSIGNED NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+
+# 좋아요 테이블 추가
+CREATE TABLE `like` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    relTypeCode CHAR(30) NOT NULL,
+    relId INT(10) UNSIGNED NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    `point` SMALLINT(1) NOT NULL
+);
+
+# 좋아요 인덱스
+ALTER TABLE `like` ADD INDEX (`relTypeCode`, `relId`, `memberId`);
+
+# 댓글 테이블 생성
+CREATE TABLE reply(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    articleId INT(10) UNSIGNED NOT NULL,
+    parentReplyId INT(10) UNSIGNED NOT NULL DEFAULT 0,
+    `body` LONGTEXT NOT NULL,
+    hitsCount INT(10) UNSIGNED NOT NULL DEFAULT 0
+);
+
+# attr 테이블 (직군 및 태그 등 불특정 데이터) 생성
+CREATE TABLE attr(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    relTypeCode CHAR(20) NOT NULL, 
+    relId INT(10) UNSIGNED NOT NULL,
+    typeCode CHAR(30) NOT NULL,
+    type2Code CHAR(30) NOT NULL,
+    `value` TEXT NOT NULL,
+    expireDate DATETIME NULL
+);
+# 변수찾는 속도 최적화 중복 금지
+ALTER TABLE attr ADD UNIQUE INDEX (relTypeCode,relId, typeCode, type2Code);
+
+# 특정 조건을 만족하는 회원 또는 게시물(기타데이터)를 빠르게 찾기 위해
+ALTER TABLE attr ADD INDEX (relTypeCode, typeCode, type2Code);
+
+	# 파일 테이블 추가
+CREATE TABLE genFile (
+  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, # 번호
+  regDate DATETIME DEFAULT NULL, # 작성날짜
+  updateDate DATETIME DEFAULT NULL, # 갱신날짜
+  delDate DATETIME DEFAULT NULL, # 삭제날짜
+  delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0, # 삭제상태(0:미삭제,1:삭제)
+  relTypeCode CHAR(50) NOT NULL, # 관련 데이터 타입(article, member)
+  relId INT(10) UNSIGNED NOT NULL, # 관련 데이터 번호
+  originFileName VARCHAR(100) NOT NULL, # 업로드 당시의 파일이름
+  fileExt CHAR(10) NOT NULL, # 확장자
+  typeCode CHAR(20) NOT NULL, # 종류코드 (common)
+  type2Code CHAR(20) NOT NULL, # 종류2코드 (attatchment)
+  fileSize INT(10) UNSIGNED NOT NULL, # 파일의 사이즈
+  fileExtTypeCode CHAR(10) NOT NULL, # 파일규격코드(img, video)
+  fileExtType2Code CHAR(10) NOT NULL, # 파일규격2코드(jpg, mp4)
+  fileNo SMALLINT(2) UNSIGNED NOT NULL, # 파일번호 (1)
+  fileDir CHAR(20) NOT NULL, # 파일이 저장되는 폴더명
+  PRIMARY KEY (id),
+  KEY relId (relId,relTypeCode,typeCode,type2Code,fileNo)
+); 
